c832276db1c64092ccb54993036cb59ee3920056
Mary Baker <meb601@gmail.com>
2020-09-04 12:09:58 -0400

    Create useLocalStorage custom hook for stretch

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,15 @@
 // write your custom hook here to control your checkout form
+export const useForm = (key, initialValue) => {
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+    };
+
+    return [values, handleChanges, handleSubmit];
+
+};
